8f25379feb130a219776403919b83e5d7e10c9b4
MCUmbrella <mcumbrella@protonmail.com>
2022-10-15 01:43:20 +0800

    ok

---
+++ src/main/java/vip/floatationdevice/ConfigUtil.java
@@ -0,0 +1,76 @@
+package vip.floatationdevice;
+
+import java.io.*;
+import java.util.Properties;
+import java.util.UUID;
+
+/**
+ * Manages 'eliza.properties'. The file is stored and read under the working directory.
+ */
+public class ConfigUtil
+{
+    static String token, channelId;
+    public static class ConfigCreationException extends RuntimeException
+    {
+        public ConfigCreationException(){super("Failed to create config file");}
+    }
+
+    public static class InvalidConfigException extends RuntimeException
+    {
+        public InvalidConfigException(){super("Config file check failed");}
+    }
+
+    public static void loadConfig() throws InvalidConfigException, ConfigCreationException
+    {
+        File configFile = new File("." + File.separator + "eliza.properties");
+
+        // create config file if it doesn't exist
+        if(!configFile.exists())
+        {
+            System.err.println("'eliza.properties' not found and a new one will be created.\nFill it with your token and channel ID, then restart the program.");
+            try
+            {
+                // copy the example config to ./config.properties
+                configFile.getParentFile().mkdirs();
+                configFile.createNewFile();
+                InputStream is = Main.class.getResourceAsStream("/eliza.properties");
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                BufferedWriter bw = new BufferedWriter(new FileWriter(configFile));
+                for(String line; (line = br.readLine()) != null; ) bw.write(line + "\n");
+                bw.flush();
+                bw.close();
+                br.close();
+                isr.close();
+                is.close();
+                System.exit(1);
+            }
+            catch(Exception e)
+            {
+                ConfigCreationException cce = new ConfigCreationException();
+                cce.initCause(e);
+                throw cce;
+            }
+        }
+
+        // load and check config file
+        try
+        {
+            Properties config = new Properties();
+            config.load(new FileInputStream(configFile));
+            token = config.getProperty("token");
+            channelId = config.getProperty("channelId");
+            // check if all required properties are set
+            if(token == null || channelId == null || token.isEmpty() || channelId.isEmpty())
+                throw new IllegalArgumentException("Null value detected");
+            // test if channelId is a valid UUID
+            UUID.fromString(channelId);
+        }
+        catch(Exception e)
+        {
+            InvalidConfigException ice = new InvalidConfigException();
+            ice.initCause(e);
+            throw ice;
+        }
+    }
+}
--- src/main/java/vip/floatationdevice/Main.java
+++ src/main/java/vip/floatationdevice/Main.java
@@ -1,20 +1,82 @@
 package vip.floatationdevice;
 
-import java.util.Scanner;
+import com.google.common.eventbus.Subscribe;
+import vip.floatationdevice.g4jbot.G4JBot;
+import vip.floatationdevice.guilded4j.event.ChatMessageCreatedEvent;
+import vip.floatationdevice.guilded4j.event.GuildedWebSocketWelcomeEvent;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 public class Main
 {
+    static String botUserId = null;
+    static Eliza e = new Eliza();
+
     public static void main(String[] args)
     {
-        Eliza e = new Eliza();
-        System.out.println(Eliza.WELCOME_MSG);
-        Scanner sc = new Scanner(System.in);
-        String s;
-        for(; ; )
-        {
-            s = sc.nextLine();
-            //System.err.println('=' + strNormalize(s));
-            System.out.println(e.getResponse(s));
-        }
+        ConfigUtil.loadConfig();
+        G4JBot b = new G4JBot(ConfigUtil.token);
+        b.setProxy(new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", 59909)));
+        b.setVerbose(true)
+                .setAutoReconnect(true)
+                .setCommandPrefix("/eliza ")
+                .registerEventListener(new Object()
+                {
+                    @Subscribe
+                    public void onLogin(GuildedWebSocketWelcomeEvent ev)
+                    {
+                        botUserId = ev.getSelf().getId();
+                        new Thread()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                try
+                                {
+                                    b.getChatMessageManager().createChannelMessage(ConfigUtil.channelId,
+                                            Eliza.WELCOME_MSG,
+                                            null,
+                                            null,
+                                            null,
+                                            null
+                                    );
+                                }
+                                catch (Exception ex)
+                                {
+                                    System.err.println("Error creating channel message: " + ex);
+                                }
+                            }
+                        }.start();
+                    }
+
+                    @Subscribe
+                    public void onChat(ChatMessageCreatedEvent ev)
+                    {
+                        if (ev.getChatMessage().getChannelId().equals(ConfigUtil.channelId) && !ev.getChatMessage().getCreatorId().equals(botUserId))
+                            new Thread()
+                            {
+                                @Override
+                                public void run()
+                                {
+                                    try
+                                    {
+                                        b.getChatMessageManager().createChannelMessage(ConfigUtil.channelId,
+                                                e.getResponse(ev.getChatMessage().getContent()),
+                                                null,
+                                                new String[]{ev.getChatMessage().getId()},
+                                                null,
+                                                null
+                                        );
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        System.err.println("Error creating channel message: " + ex);
+                                    }
+                                }
+                            }.start();
+                    }
+                })
+                .connectWebSocket();
     }
 }
